3683abad02c814aa33fa52c6b693417f4ac1a467
KleKlai <magallenmaynards@gmail.com>
2023-12-26 15:02:37 +0800

    Added register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Link href="https://clashodds.tech/r/C-6A9BDEE2">Register Now</Link>
+    </div>
+  )
+}
