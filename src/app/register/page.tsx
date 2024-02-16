d4c78e8a40677abaee0eac5ae9e651e074dcc831
KleKlai <magallenmaynards@gmail.com>
2024-02-17 00:59:10 +0800

    chore: change link

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
     <div className="flex h-screen justify-center items-center bg-[url('https://images.unsplash.com/photo-1463778476191-ec665605a4d4?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center shadow-lg">
       <div>
         <Link
-          href="https://clashodds.tech/r/C-6A9BDEE2"
+          href="https://gamepinoy.com/r/C-6A9BDEE2"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full shadow-lg"
         >
           Register Now
